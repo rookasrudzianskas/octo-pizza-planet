0a61a80178bb26398798b3236a3044a2cbd430de
Rokas <rookas.rudzenskas@gmail.com>
2021-04-04 14:49:39 +0300

    Initial

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
       <app-header/>
     <Home />
+    <Menu />
     </div>
 </template>
 
 <script>
 import Header from './components/Header';
 import Home from './components/Home';
+import Menu from './components/Menu';
 export default {
   name: 'App',
   components: {
     appHeader: Header,
     Home,
+    Menu,
   }
 };
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="menu_wrapper">
+    <div class="menu">
+      <h3>~ Authentic handmade pizza~</h3>
+      <table>
+        <tbody>
+        <tr>
+          <td>
+            <strong>~ Pepperoni ~</strong>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, dolores?</small>
+          </td>
+        </tr>
+        <tr>
+          <td>9"</td>
+          <td>$6.95</td>
+          <td>
+            <button type="button" class="btn_green">+</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<style scoped>
+h3 {
+  text-align: center;
+}
+
+.menu_wrapper {
+  display: flex;
+  flex-direction: column;
+
+}
+
+.menu {
+  background-color: #f1e6da;
+  border-radius: 3px;
+  height: 100vh;
+  padding: 10px;
+  margin: 10px;
+}
+
+@media screen and (min-width: 900px){
+  .menu_wrapper {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  .menu {
+    width: 65vw;
+  }
+}
+</style>
