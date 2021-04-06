5a62771800a5c227d9db3c75fa2f24326237fdde
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 11:57:18 +0300

    Rokas

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login_wrapper">
+    <foorm>
+      <div>
+        <label for="email">Email address</label>
+        <input type="email" id="email" placeholder="Enter the @email">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Password">
+      </div>
+      <button type="button" class="btn_green">Sign in</button>
+    </foorm>
+  </div>
+</template>
