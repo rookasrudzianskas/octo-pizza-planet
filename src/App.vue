1db1bb0ca9153313f3576bedb526f70cd026a47b
Rokas <rookas.rudzenskas@gmail.com>
2021-04-06 22:11:02 +0300

    Rokas

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,12 @@
 
 <script>
 import Header from './components/Header';
-// import Home from './components/Home';
-// import Admin from './components/Admin'
+
 export default {
   name: 'App',
   components: {
     appHeader: Header,
-    // Home,
-    // Admin,
+
   }
 };
 </script>
