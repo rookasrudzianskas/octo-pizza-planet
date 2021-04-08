dcbea61fcd1852bfc63c8952d6091f12b76508b4
Rokas <rookas.rudzenskas@gmail.com>
2021-04-08 14:08:44 +0300

    Rokas

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+import Home from './components/Home'
+import Menu from './components/Menu'
+import Delivery from './components/Delivery'
+// import OrderingGuide from './components/OrderingGuide'
+import About from './components/About'
+import Admin from './components/Admin'
+import Contact from './components/Contact'
+import OrderingGuide from './components/OrderingGuide'
+import History from './components/History'
+
+
+
+
+
+export const routes = [
+    {path: '/', name: 'homeLink', component: Home },
+    {path: '/menu', name: 'menuLink',component: Menu },
+    {path: 'contact', name: 'contactLink', component: Contact},
+    {
+        path: '/admin',
+        name: 'adminLink',
+        component: Admin,
+        beforeEnter: (to, from, next) => {
+            alert('This area is for authorized users only, please login to continue');
+            next()
+        }
+    },
+    {path: '/about', name: 'aboutLink', component: About, children: [
+            {path: '/history', name: 'historyLink', component: History},
+            {path: '/delivery', name: 'deliveryLink', component: Delivery},
+            {path: '/ordering-guide', name: 'orderingGuideLink', component: OrderingGuide},
+        ]},
+    {path: '*', redirect: '/'}
+]
